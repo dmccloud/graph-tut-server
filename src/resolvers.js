68d3a74e894d791680ec29e968e37a1b09ffdeea
dhmcclou22 <dhmcclou@asu.edu>
2021-12-15 13:57:38 -0700

    finished with tut now time to deploy

---
+++ src/resolvers.js
@@ -0,0 +1,50 @@
+const resolvers = {
+  Query: {
+    tracksForHome: (_, __, { dataSources }) => {
+      console.log(_);
+      return dataSources.trackAPI.getTracksForHome();
+    },
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id);
+    },
+    module: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getModule(id);
+    },
+  },
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      console.log(authorId);
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+    modules: (parent, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(parent.id);
+    },
+  },
+  Module: {
+    modulesFromId: (parent, _, { dataSources }) => {
+      return dataSources.trackAPI.getModuleFromId(parent.trackId, parent.id);
+    },
+  },
+};
+
+module.exports = resolvers;
